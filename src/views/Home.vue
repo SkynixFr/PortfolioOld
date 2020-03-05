06f52175febd7a1fe6344d6c98b9e78f2acf945f
Skynix <romain.day.ludovic@gmail.com>
2020-03-05 14:02:29 +0100

    update(home page): add image

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,24 @@
 <template>
   <v-container fluid>
-    <v-row align="center" justify="center" style="height: 750px;">
-      <v-col cols="12" sm="12" md="10">
-        <v-row>
-          <v-col cols="12" sm="12" md="12">
-            <p class="headline-text">Hi, I'm Romain</p>
-          </v-col>
-          <v-col cols="12" sm="12" md="10" offset-md="1">
-            <blockquote class="blockquote-text">
-              Currently in a vocational Bachelor’s degree specialized in Mobile
-              and Web Development, I am looking for an internship from May to
-              July 2020. Before my Bachelor, I attended a 2-year technical
-              degree in Software Solutions and Trades Applications. I work very
-              well in a team environment, I am a self- taught person who loves
-              challenges. At the moment, I am working on a school project in
-              order to create a new social network.
-            </blockquote>
-          </v-col>
-        </v-row>
-        <!-- <v-row>
-        <v-col cols="10" md="6" offset-md="3">
-          <p class="display-1 font-weight-light text-center">
-            Profile
-          </p>
+    <v-row no-gutters align="center" justify="center" style="height: 750px;">
+      <v-col cols="12" sm="12" md="7">
+        <v-col cols="12" sm="12" md="12">
+          <p class="headline-text">Hi, I'm Romain</p>
         </v-col>
-        <v-col cols="10" md="6" offset-sm="3">
-          <blockquote class="blockquote">
+        <v-col cols="12" sm="12" md="10" offset-md="1">
+          <blockquote class="blockquote-text">
             Currently in a vocational Bachelor’s degree specialized in Mobile
             and Web Development, I am looking for an internship from May to July
             2020. Before my Bachelor, I attended a 2-year technical degree in
-            Software Solutions and Trades Applications. I work very well in team
-            environment, I am self- taught person who loves challenges. At the
-            moment, I am working on a school project in order to create a new
-            social network.
+            Software Solutions and Trades Applications. I work very well in a
+            team environment, I am a self- taught person who loves challenges.
+            At the moment, I am working on a school project in order to create a
+            new social network.
           </blockquote>
         </v-col>
-      </v-row> -->
+      </v-col>
+      <v-col cols="12" sm="12" md="4">
+        <v-img src="images/home.png"></v-img>
       </v-col>
     </v-row>
   </v-container>
