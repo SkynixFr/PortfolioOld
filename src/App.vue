33a5a8de3f555668b76f2d819eed6c17f29c790b
Skynix <romain.day.ludovic@gmail.com>
2020-03-06 18:46:06 +0100

    update(footer): new links

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,16 @@
       <v-col class="text-center" cols="12">
         {{ new Date().getFullYear() }} — Copyright &copy;
         <span> Day Romain</span>
+        <v-spacer></v-spacer>
+        <v-btn icon href="https://github.com/SkynixFr">
+          <v-icon>fab fa-github</v-icon>
+        </v-btn>
+        <v-btn icon href="https://twitter.com/SkynixFr">
+          <v-icon>mdi-twitter</v-icon>
+        </v-btn>
+        <v-btn icon href="https://www.linkedin.com/in/romain-day-a02229178/">
+          <v-icon>mdi-linkedin</v-icon>
+        </v-btn>
       </v-col>
     </v-footer>
   </v-app>
